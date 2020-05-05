a2616b79eec7a9f275e3e4c8329ca46e88c34e9a
Kyle Maxwell <krmaxwell@gmail.com>
2020-05-05 17:14:02 -0500

    Initial creation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name="xorthis",
+    version="0.1",
+    packages=find_packages(),
+    python_requires="3.5",
+
+    # Metadata
+    author="Kyle Maxwell",
+    author_email="krmaxwell@gmail.com",
+    description="Set of functions for XOR cryptanalysis",
+    url="https://github.com/krmaxwell/xorthis"
+)
